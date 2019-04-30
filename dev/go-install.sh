190317ad96b9bec7d30db563b6bb2958668d84cd
prince <prince18pat@gmail.com>
2019-04-30 23:36:34 +0900

    add launch script

--- dev/go-install.sh
+++ dev/go-install.sh
@@ -7,3 +7,50 @@
 # $GOMOD_ROOT is set, under that directory.
 
 all_oss_commands=" gitserver query-runner gihub-proxy management-console searcher frontend repo-updater symbols "
+
+# GOMOD_ROOT is the directory from which `go install` commands are run. It should contain a go.mod
+# file. The go.mod file may be updated as a side effect of updating the dependencies before the `go
+# install`.
+GOMOD_ROOT=${GOMOD_ROOT:-$PWD}
+echo >&2 "Running \`go install\` from $GOMOD_ROOT"
+
+# handle options
+verbose=false
+while getopts 'v' o; do
+  case $o in
+  v) verbose=true;;
+  \?) echo >&2 "usage: go-install.sh [-v] [commands]"
+      exit 1
+      ;;
+  esac
+done
+shift $(expr $OPTIND - 1)
+
+# check provided commands
+ok=true
+case $# in
+0) commands=$all_oss_commands;;
+*) commands=" $* "
+  for cmd in $commands; do
+    case $all_oss_commands in
+    *" $cmd "*) ;;
+    *) echo >&2 "Unknown command: $cmd"
+       ok=false
+       ;;
+    esac
+  done
+  ;;
+esac
+
+$ok || exit 1
+
+mkdir -p .bin
+export GOBIN=$PWD/.bin
+export GO111MODULE=on
+
+INSTALL_GO_PKGS="github.com/mattn/goreman \
+github.com/sourcegraph/docsite/cmd/docsite \
+github.com/google/zoekt/cmd/zoekt-archive-index \
+github.com/google/zoekt/cmd/zoekt-sourcegraph-indexserver \
+github.com/google/zoekt/cmd/zoekt-webserver \
+"
--- dev/launch.sh
+++ dev/launch.sh
@@ -41,3 +41,36 @@ export PGSSLMODE=disable
 
 # Default to "info" level debugging, and "condensed" log format (nice for human readers)
 export SRC_LOG_LEVEL=${SRC_LOG_LEVEL:-info}
+export SRC_LOG_FORMAT=${SRC_LOG_FORMAT:-condensed}
+export GITHUB_BASE_URL=http://127.0.0.1:3180
+export SRC_REPOS_DIR=$HOME/.sourcegraph/repos
+export INSECURE_DEV=1
+export GOLANGSERVER_SRC_GIT_SERVERS=host.docker.internal:3178
+
+
+# webpack-dev-server is a proxy running on port 3080 that (1) serves assets, waiting to respond
+# until they are built and (2) otherwise proxies to nginx running on port 3081 (which proxies to
+# sourcegraph running on port 3082). That is why Sourcegraph listens on 3081 despite the externalURL
+# having port 3080.
+export SRC_HTTP_ADDR=":3082"
+export WEBPACK_DEV_SERVER=1
+
+# WebApp
+export NODE_ENV=development
+export NODE_OPTIONS="--max_old_space_size=4096"
+
+# Make sure chokidar-cli is installed in the background
+printf >&2 "Currently installing Yarn and Go dependencies...\n\n"
+yarn_pid=''
+[ -n "${OFFLINE-}"] || {
+  yarn --no-progress &
+  yarn_pid="$!"
+}
+
+if ! ./dev/go-install.sh; then
+  # let yarn finish, otherwise we get Yarn diagnostics AFTER the
+  # actual reason we're failing.
+  wait
+  echo >&2 "WARNING: go-install.sh failed, some builds may have failed."
+  exit 1
+fi
