f8ff6f6a3c3c2432cdd498a5b09da89131315db9
prince <prince18pat@gmail.com>
2019-05-12 21:40:30 +0900

    add web config and package

--- web/gulpfile.ts
+++ web/gulpfile.ts
@@ -1,1 +1,11 @@
-import gulp from 'gulp'
+import log from 'fancy-log'
+import {Stats} from 'webpack'
+
+
+const WEBPACK_STATS_OPTIONS: Stats.ToStringOptions & { colors?: boolean } = {}
+
+const logWebpackStats = (stats: Stats) => log(stats.toString(WEBPACK_STATS_OPTIONS))
+
+export async function webpack(): Promise<void> {
+
+}
---
+++ web/webpack.config.ts
@@ -0,0 +1,4 @@
+// tslint:disable-next-line:no-reference
+
+import MiniCssExtractPlugin from 'mini-css-extract-plugin'
+
