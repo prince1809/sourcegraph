0dbec2a71ac4c8647f0faa8eb8700cda581b3f47
prince <prince18pat@gmail.com>
2019-05-13 00:14:48 +0900

    add typescript config and add package

--- web/webpack.config.ts
+++ web/webpack.config.ts
@@ -1,4 +1,5 @@
 // tslint:disable-next-line:no-reference
+/// <reference path="../shared/src/types/terser-webpack-plugin/index.d.ts" />
 
 import MiniCssExtractPlugin from 'mini-css-extract-plugin'
-
+import OptimizeCssAssetsPlugin from 'optimize-css-assets-webpack-plugin'
