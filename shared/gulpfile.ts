9a02b9429be4847c4f602a2f6e34df75ca454185
prince <prince18pat@gmail.com>
2019-05-15 02:02:11 +0900

    add gulp config

--- shared/gulpfile.ts
+++ shared/gulpfile.ts
@@ -1,16 +1,41 @@
+import { generateNamespace } from '@gql2ts/from-schema'
+import { DEFAULT_OPTIONS, DEFAULT_TYPE_MAP } from '@gql2ts/language-typescript'
+import {buildSchema, graphql, IntrospectionQuery, introspectionQuery} from 'graphql';
 import gulp from 'gulp'
 import {readFile} from 'mz/fs'
+import path from 'path';
+import {resolveConfig} from 'prettier'
 
+const GRAPHQL_SCHEMA_PATH = path.join(__dirname, '../cmd/frontend/graphqlbackend/schema.graphql')
 
-export async function watchGraphQLTypes() {
+export async function watchGraphQLTypes(): Promise<void> {
     await new Promise<never>((resolve, reject) => {
-        gulp.watch()
+        gulp.watch(GRAPHQL_SCHEMA_PATH, graphQLTypes).on('error', reject)
     })
 }
 
 /** Generates the Typescript types for the GraphQL schema */
 export async function graphQLTypes(): Promise<void> {
-    const schemaStr = await readFile()
+    const schemaStr = await readFile(GRAPHQL_SCHEMA_PATH, 'utf8')
+    const schema = buildSchema(schemaStr)
+    const result = (await graphql(schema, introspectionQuery)) as { data: IntrospectionQuery }
+
+    const formatOptions = (await resolveConfig(__dirname, {config: __dirname + '/../prettier.config.js'}))
+    const typings =
+        'export type ID = string\n\n' +
+        generateNamespace(
+            '',
+            result,
+            {
+                typeMap: {
+                    ...DEFAULT_TYPE_MAP,
+                    ID: 'ID',
+                },
+            },
+            {
+
+            }
+        )
 }
 
 /**
